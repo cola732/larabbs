d8e4f06a4b2d8ef1837290af502243b7a173e764
quanyu <wqy1032@163.com>
2018-12-06 16:46:34 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -1,8 +1,19 @@
 <?php
 
 namespace App\Models;
+use App\User;
 
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
